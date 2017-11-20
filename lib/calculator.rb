6f6e1b0237eb29b607838565f9ceb676bbc7e965
Josh Aronson <jparonson@gmail.com>
2017-11-19 21:59:29 -0600

    Add calculator class

---
+++ lib/calculator.rb
@@ -0,0 +1,40 @@
+class Calculator
+  UnrecognizedInputError = Class.new(StandardError)
+
+  attr_reader :stack
+
+  def initialize
+    @stack = []
+  end
+
+  def handle_input(input)
+    [].tap do |output|
+      input.split(' ').each do |part|
+        handle_part(part)
+      end
+    end
+  end
+
+  private
+
+  def handle_part(part)
+    case part
+    when /^-?\d+$/
+      @stack.push(part.to_i)
+    when /^-?[0-9]+(\.[0-9]+)?$/
+      @stack.push(part.to_f)
+    when /^[-+\/*]$/
+      output.push(operate(part))
+    when /^state$/
+      output.push(@stack.inspect)
+    else
+      raise UnrecognizedInputError.new
+    end
+  end
+
+  def operate(operator)
+    result = @stack.reduce(operator.to_sym)
+    @stack = []
+    result
+  end
+end
